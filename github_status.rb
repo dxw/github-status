2288ea76f9d1e27522f8dae77c7f09f508227adc
Maria Herrera <maria@dxw.com>
2020-07-31 12:52:44 +0100

    S3 bucket and logic added
    The app didn't work properly once it was on Heroku due to the yaml file that
    stores the information about github status being re-started from default
    every time Heroku kicked off the scheduler on a one-off dyno.
    
    This commit solves that problem by storing the yaml file on an AWS S3 bucket
    that Heroku will be able to access to load the content and update the status
    every time it runs on the scheduler.

--- github_status.rb
+++ github_status.rb
@@ -1,10 +1,12 @@
-require 'json'
-require 'yaml/store'
-require 'yaml'
-require 'net/http'
-require 'dotenv/load'
+require "aws-sdk-s3"
+require "json"
+require "yaml/store"
+require "yaml"
+require "net/http"
+require "dotenv/load"
 
-url = 'https://kctbh9vrtdwd.statuspage.io/api/v2/status.json'
+data_file = "github_status.yml"
+url = "https://kctbh9vrtdwd.statuspage.io/api/v2/status.json"
 
 response = Net::HTTP.get_response URI(url)
 
@@ -12,15 +14,22 @@ return unless response.code.to_i == 200
 
 github_status = JSON.parse(response.body)
 
-previous_file = YAML.load_file('github_status.yml')
-previous_indicator = previous_file['indicator']
+s3 = Aws::S3::Resource.new(region: ENV["AWS_REGION"])
+obj = s3.bucket(ENV["AWS_BUCKET"]).object(data_file)
+resp = obj.get.body.read
 
-current_indicator = github_status['status']['indicator']
-current_description = github_status['status']['description']
+previous_file = YAML.load(resp)
+previous_indicator = previous_file["indicator"]
+
+current_indicator = github_status["status"]["indicator"]
+current_description = github_status["status"]["description"]
 
 if current_indicator != previous_indicator
-  new_status = YAML.dump(github_status['status'])
-  File.open('github_status.yml', 'w') {|f| f.write new_status }
+  new_data_file = {
+    "indicator" => current_indicator,
+    "description" => current_description
+  }
+  s3.bucket(ENV["AWS_BUCKET"]).put_object(key: data_file, body: YAML.dump(new_data_file))
 
   if current_indicator == "none"
     message = "GitHub incident has now been resolved. #{current_description}. For further info, visit https://www.githubstatus.com"
@@ -29,7 +38,6 @@ if current_indicator != previous_indicator
   else
     message = "GitHub status has changed, from #{previous_indicator} issues to #{current_indicator} issues (#{current_description}). For further info, visit https://www.githubstatus.com"
   end
-
   Net::HTTP.post URI(ENV["SLACK_WEBHOOK_URL"]),
    { text: message }.to_json,
    "Content-Type" => "application/json"
